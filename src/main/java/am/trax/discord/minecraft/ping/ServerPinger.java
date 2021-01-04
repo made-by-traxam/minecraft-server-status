31ae378fadce516a5d7a60d64eca34dce38424fc
traxam <traxam@trax.am>
2021-01-04 23:52:58 +0100

    fix: split port from server addresses

--- src/main/java/am/trax/discord/minecraft/ping/ServerPinger.java
+++ src/main/java/am/trax/discord/minecraft/ping/ServerPinger.java
@@ -1,6 +1,7 @@
 package am.trax.discord.minecraft.ping;
 
 import br.com.azalim.mcserverping.MCPing;
+import br.com.azalim.mcserverping.MCPingOptions;
 import br.com.azalim.mcserverping.MCPingResponse;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,16 +20,30 @@ public class ServerPinger {
      * @return the status of the pinged server.
      */
     public static ServerStatus ping(String address) {
+        MCPingOptions options = getPingOptions(address);
         Instant timestamp = Instant.now();
         MCPingResponse ping = null;
 
         try {
-            ping = MCPing.getPing(address);
+            ping = MCPing.getPing(options);
         } catch (IOException e) {
-            //TODO differentiate between different types of exceptions, i.e. UnknownHostException
+            //TODO differentiate between different types of exceptions, i.e. UnknownHostException,
             log.error("Error while pinging Minecraft server", e);
         }
 
         return new ServerStatus(address, ping, ping != null, timestamp);
+    }
+
+    private static MCPingOptions getPingOptions(String address) {
+        int colonIndex = address.indexOf(':');
+        if (colonIndex == -1) {
+            return MCPingOptions.builder().hostname(address).build();
+        } else {
+            String hostname = address.substring(0, colonIndex);
+            String portString = address.substring(colonIndex + 1);
+            int port = Integer.parseInt(portString); //TODO handle NumberFormatException
+
+            return MCPingOptions.builder().hostname(hostname).port(port).timeout(10000).build();
+        }
     }
 }
